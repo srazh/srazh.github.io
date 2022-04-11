75348f2f5f7a4aa127737706f8a17bbeeb0310b2
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-04-11 16:24:37 -0400

    add typewriter

---
+++ src/components/Container.js
@@ -0,0 +1,8 @@
+export default function Container(){
+    return (
+        <div  class="container-fluid">
+            <img src="header.jpg" style="width:100% height=500px"></img>
+        </div>
+
+    )
+}
