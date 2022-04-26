cc48ff11b46db4960aae6d715c023228a421ea4a
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-04-26 12:06:03 -0400

    regroup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import './App.css';
-//import Navbar from "./components/navbar/NavbarElements";
+import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
 import Work from './components/pages/Work';
@@ -10,13 +10,16 @@ import Interests from './components/pages/Interests';
 
 export default function App() {
     return (
+        <div>
+            <Navbar />
         <Router>
-        <Routes>
+        <Routes> 
             <Route path='/' element={<Home/>} />
             <Route path='/Work' element={<Work/>} />
             <Route path='/Play' element={<Play/>} />
             <Route path='/Interests' element={<Interests/>} />
         </Routes>
         </Router>
+        </div>
     ); 
 }
