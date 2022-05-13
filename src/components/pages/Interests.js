af22cc7c4e473c18f98cdd0b94dcf2c50de28cd1
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-05-13 12:53:14 -0400

    inerest cards

--- src/components/pages/Interests.js
+++ src/components/pages/Interests.js
@@ -1,90 +1,37 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-import Button from 'react-bootstrap/Button'
+import Card from "../pages/home/Card";
+import {Grid } from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 
-
+const useStyles = makeStyles({
+  gridContainer: {
+    paddingLeft:"40px",
+    paddingRight: "40px"
+  }
+})
 
 export default function Interests() {
+  const classes = useStyles();
+
   return (
-    <div>
-    <img src={"./assets/etsy.jpg"} className="interests-pics" alt="etsy" width="200" height="300"/>
-    <img src={"./assets/foodie.jpg"} className="interests-pics" alt="foodie" width="200" height="300"/>
-    <img src={"./assets/music.jpg"} className="interests-pics" alt="music" width="200" height="300"/>
-    <img src={"./assets/spotify.png"} className="interests-pics" alt="spotify" width="200" height="300"/>
+    <Grid container spacing= {4} classame={classes.gridContainer} justify="center">>
+      <Grid item xs={12} sm={6} md={4}>
+      <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+      <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+      <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+      <Card />
+      </Grid>
 
+    </Grid>
+   
 
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="./assets/concerts.jpg" width="200" height="300"/>
-  <Card.Body>
-    <Card.Title>Concerts</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-
-
-    </div>
 
   );
 }
 
-
-
-/*
-const Interests = () => {
-    <CardGroup>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-</CardGroup>
-}
-
-export default Interests;
-
-
-
-
-
-
-*/
