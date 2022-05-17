15e873eeeef1d786f5d993abc7ae1f214067e217
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-05-17 13:30:24 -0400

    add image gallery

--- src/components/pages/Play.js
+++ src/components/pages/Play.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import antelopeCanyon from "../../assets/Play/antelopeCanyon.jpg";
+import ballet from "../../assets/Play/ballet.jpg";
+import goldfish from "../../assets/Play/goldfish.jpg";
+import jellyfish from "../../assets/Play/jellyfish.jpg";
 
-const Play = () => {
-return (
-	<h1>Play</h1>
-);
-};
+export default function Play() {
+	return (
+		<div class="row">
+		<div class="column">
+			<img src={antelopeCanyon} className="play-pics" alt="antelope canyon" onclick="myFunction(this);" width="300" height="400"/>
+		</div>
+		<div class="column">
+			<img src={ballet} className="play-pics" alt="antelope canyon" onclick="myFunction(this);" width="300" height="400"/>
+		</div>
+		<div class="column">
+			<img src={goldfish} className="play-pics" alt="antelope canyon" onclick="myFunction(this);" width="300" height="400"/>
+		</div>
+		<div class="column">
+			<img src={jellyfish} className="play-pics" alt="antelope canyon" onclick="myFunction(this);" width="300" height="400"/>
+		</div>
+		</div>
 
-export default Play;
+	);
+}
+
+
+
+
+
+
+
+
+
+
