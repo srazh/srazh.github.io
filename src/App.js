e481bc5779d4cea6b69994732101712c56e209c6
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-07-20 23:41:27 -0400

    minor edits
    minor changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+<<<<<<< Updated upstream
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+=======
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+>>>>>>> Stashed changes
 import Home from './components/pages/Home';
 import Work from './components/pages/Work';
 import Play from './components/pages/Play';
 import Interests from './components/pages/Interests';
 import Container from './components/Container';
+<<<<<<< Updated upstream
 import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
 import "./index.css";
@@ -23,3 +28,23 @@ export default function App() {
         </Router>
     ); 
 }
+=======
+import NabvBar from './components/Navbar';
+import { Footer } from 'react-bootstrap/lib/Modal';
+
+export default function App() {
+  return (
+    <Router>
+      <Container />
+      <NabvBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Work" element={<Work />} />
+        <Route path="/Play" element={<Play />} />
+        <Route path="/Interests" element={<Interests />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+>>>>>>> Stashed changes
